963e89e658ec001aca324aee25da0128b292d157
u8yes <108450018+u8yes@users.noreply.github.com>
2022-12-05 18:18:37 +0900

    Update 15.write_image1.py

--- src/chap02/15.write_image1.py
+++ src/chap02/15.write_image1.py
@@ -10,5 +10,5 @@ params_png = [cv2.IMWRITE_PNG_COMPRESSION, 9]       # PNG 압축 레벨 설정
 cv2.imwrite("images/write_test1.jpg", image)       # 디폴트는 95
 cv2.imwrite("images/write_test2.jpg", image, params_jpg) # 지정 화질로 저장
 cv2.imwrite("images/write_test3.png", image, params_png)
-cv2.imwrite("iamges/write_test4.bmp", image)         # BMP 파일로 저장
+cv2.imwrite("images/write_test4.bmp", image)         # BMP 파일로 저장
 print("저장 완료")
