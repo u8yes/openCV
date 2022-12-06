5e3efe613d46eaafadbd31ec08d07b6f24393931
u8yes <108450018+u8yes@users.noreply.github.com>
2022-12-06 18:46:36 +0900

    64

--- src/chap03/11_mat_gemm.py
+++ src/chap03/11_mat_gemm.py
@@ -5,10 +5,16 @@ src2 = np.array([1, 2, 3, 4, 5, 6], np.float32).reshape(2, 3)
 src3 = np.array([1, 2, 1, 2, 1, 2], np.float32).reshape(3, 2) # 3x2 행렬 선언
 alpha, beta = 1.0, 1.0
 
+# 행렬의 내적을 계산해주는 함수(gemm)
 dst1 = cv2.gemm(src1, src2, alpha, None, beta, flags=cv2.GEMM_1_T)
+# flags=cv2.GEMM_1_T, 1은 첫번째 인수(src1)를 가리킴, T는 항상 .T나 T는 전치행렬을 의미한다.
+# [[1 1]
+#  [2 2]
+#  [3 3]]
+# 내적곱을 src1과 src2와 해줌.
 dst2 = cv2.gemm(src1, src2, alpha, None, beta, flags=cv2.GEMM_2_T)
 dst3 = cv2.gemm(src1, src3, alpha, None, beta)
 
-titles = ['src1','src1','src1','dst1','dst2','dst3']
+titles = ['src1','src2','src3','dst1','dst2','dst3']
 for title in titles:
     print("[%s] = \n%s\n" % (title, eval(title)))
--- src/chap03/13_equation.py
+++ src/chap03/13_equation.py
@@ -4,11 +4,14 @@ data = [ 3, 0, 6, -3, 4, 2, -5,-1, 9]
 m1 = np.array(data, np.float32).reshape(3,3)
 m2 = np.array([36, 10, 28], np.float32)
 
-ret, inv = cv2.invert(m1, cv2.DECOMP_LU)                # 역행렬 계산
-if ret:
-    dst1 = inv.dot(m2)
-    dst2 = cv2.gemm(inv, m2, 1, None, 1)
+# 역행렬 계산
+ret, inv = cv2.invert(m1, cv2.DECOMP_LU)
+# decomposition(분해) # DECOMP_LU 는 디폴트 옵션
+if ret: # 역행렬이 존재할 경우
+    dst1 = inv.dot(m2) # 넘파이에 의한 m2 내적을 곱함.
+    dst2 = cv2.gemm(inv, m2, 1, None, 1) # cv에 의한 내적 계산.
     ret, dst3 = cv2.solve(m1, m2, cv2.DECOMP_LU)
+    # 연립방정식에 대한 풀이 결과
 
     print("[inv] = \n%s\n" % inv)
     print("[dst1] =", dst1.flatten())
