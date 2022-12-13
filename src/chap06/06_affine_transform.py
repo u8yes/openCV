936a3d1c103178fbf8070fef7578d43d8ce8688e
u8yes <108450018+u8yes@users.noreply.github.com>
2022-12-13 13:15:25 +0900

    Update 06_affine_transform.py

--- src/chap06/06_affine_transform.py
+++ src/chap06/06_affine_transform.py
@@ -1,16 +1,20 @@
 import numpy as np, cv2
 
-image = cv2.imread("06_images/", cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("06_images/affine.jpg", cv2.IMREAD_GRAYSCALE)
 if image is None: raise Exception("영상 파일 읽기 에러")
+# image size 320x330
 
+center = (200, 200) # 중심점
+angle, scale = 30, 1
+size = image.shape[::-1]
 
+pt1 = np.array([(30, 70), (20, 240), (300, 110)], np.float32) # float32는 .0이 붙게 만듦.
+pt2 = np.array([(120, 20), (10, 180), (280, 260)], np.float32) # 1차원
 
+aff_mat = cv2.getAffineTransform(pt1, pt2) # affine 행렬 반환
+rot_mat = cv2.getRotationMatrix2D(center, angle, scale)
 
-
-
-
-
-
+cv2.warpAffine()
 
 
 
